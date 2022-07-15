707b6099a24568e5d54ba71290bf1657e7bd77a8
SonaruIsuge <bread511206@gmail.com>
2022-07-15 15:57:43 +0800

    StartTime countdown

--- Assets/Sonaru Developer/GameController/StageStartState.cs
+++ Assets/Sonaru Developer/GameController/StageStartState.cs
@@ -1,23 +1,25 @@
 ﻿
+using SonaruUtilities;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 public class StageStartState : IState
 {
     public GameController Controller { get; set; }
+    private SimpleTimer startTimer;
     
     public void OnStateEnter(GameController controller)
     {
         Controller = controller;
         
         Controller.MapControl.LoadBlockAssetData();
-        
-        Controller.DelayDo(()=>Controller.ChangeState(StateEnum.MainPlay), 3);
+        startTimer = new SimpleTimer(3);
+        // Controller.DelayDo(()=>Controller.ChangeState(StateEnum.MainPlay), 3);
     }
 
     public void OnStateStay()
     {
-        
+        if (startTimer.IsFinish) Controller.ChangeState(StateEnum.MainPlay);
     }
 
     public void OnStateExit()
