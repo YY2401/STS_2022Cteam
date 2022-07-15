5d0e493d20cf92c591fdeafb1edf14f15446c572
darkcatneko <darkcat7126@gmail.com>
2022-07-15 13:18:49 +0800

    two pad

---
+++ Assets/darkcat/GamePadRes.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using System.Linq;
+
+public class PlayerManager
+{
+    public static Dictionary<Gamepad, GamePadRes> playerList = new Dictionary<Gamepad, GamePadRes>();
+}
+public class GamePadRes : MonoBehaviour
+{
+    public Gamepad _gamepad;
+    public enum Check
+    {
+        circle, cross
+    }
+    public Check check;
+    public bool isChecked = false;
+    public DpadControl LeftDpad;
+    void Start()
+    {
+        PlayerManager.playerList.Clear();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isChecked)
+        {
+            foreach (Gamepad g in Gamepad.all)
+            {
+                if (PlayerManager.playerList.Keys.Contains(g)) continue;
+                if (check == Check.circle)
+                {
+                    if (g.buttonEast.isPressed)
+                    {
+                        _gamepad = g;
+                        isChecked = true;
+                        PlayerManager.playerList.Add(g, this);
+                        LeftDpad = _gamepad.dpad;
+                        //TwoControllerRegistry.instance.P12.enabled = true;
+                        Debug.Log(g.name + ": " + check.ToString());
+                        //AudioController.Instance.SpawnSFX(SFXType.CirclePair);
+                        return;
+                    }
+                }
+                else if (check == Check.cross)
+                {
+                    if (g.buttonSouth.isPressed)
+                    {
+                        _gamepad = g;
+                        isChecked = true;
+                        PlayerManager.playerList.Add(g, this);
+                        LeftDpad = _gamepad.dpad;
+                        //TwoControllerRegistry.instance.P34.enabled = true;
+                        Debug.Log(g.name + ": " + check.ToString());
+                        //AudioController.Instance.SpawnSFX(SFXType.CrossPair);
+                        return;
+                    }
+                }
+            }
+            return;
+        }
+    }
+}
