e3827612b7d9caab1ac2513f6e60011804382eb2
darkcatneko <darkcat7126@gmail.com>
2022-07-15 15:44:28 +0800

    End Ontrigger

---
+++ Assets/darkcat/OntriggerEnterEnd.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OntriggerEnterEnd : MonoBehaviour
+{
+    public NowColor EndColor;
+    public Player EndPlayer;
+    private void OnTriggerStay(Collider Player)
+    {
+        if (Player.gameObject.name == "Character_1_mod"|| Player.gameObject.name == "Character_2_mod")
+        {
+            if (Player.GetComponent<Move>().ThisPlayer == EndPlayer&& Player.GetComponent<Move>().ThisColor == EndColor)
+            {
+                Player.GetComponent<Move>().IsToEnd = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider Player)
+    {
+        if (Player.gameObject.name == "Character_1_mod" || Player.gameObject.name == "Character_2_mod")
+        {            
+            Player.GetComponent<Move>().IsToEnd = false;            
+        }
+    }
+}
